38c2cddf3b4f876c593faf9abbd7cf66dc61bf6e
rkpeltier <rkpeltier@gmail.com>
2019-01-25 13:44:55 -0800

    updated html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,56 @@
+$(document).ready(function() {
 
-//Giphy query url
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=WrV5rnOz4iJHVQjfvzOTyxiOIoLecRKA&limit=5";
+//Some starter bands array
+var bands = ["The Beatles", "Blondie", "Queen", "The Doors"];
+  
+//Make gifs show up
+function renderGifs() {
 
-//Ajax
-$.ajax({
+  //Giphy query url
+  var queryURL = "https://api.giphy.com/v1/gifs/search?=" + band + "api_key=WrV5rnOz4iJHVQjfvzOTyxiOIoLecRKA&limit=10";
+  
+  //Ajax
+  $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    $("#display-gifs").empty();
     
+    //Add function for adding gifs by clicking buttons
+    var imageUrl = response.data.image_original_url;
+    var bandImage = $("<img>");
+    
+    bandImage.attr("src", imageUrl);
+    bandImage.attr("atl", "band image");
+  
   }); 
+}
+
 
   //Buttons
+  function renderButtons() {
+    $("#buttons-view").empty();
 
-  //Search + button
+    for(var i = 0; i < bands.length; i++) {
+      var a = $("<button>");
+      a.addClass("bands-button");
+      a.attr("data-name", bands[i]);
+      a.text(bands[i]);
+      $("#buttons-view").append(a);
+    }
+  }
 
-  //start and stop gifs
+  //Add new band button
+  $("#add-band").on("click", function(event) {
+    event.preventDefault(); 
+    var band = $("#band-input").val().trim();
+    bands.push(band);
+    renderButtons();
+
+  });
+
+  renderButtons();
+  renderGifs();
+
+});
